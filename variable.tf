13c65fbf7568a69e10ab2cf23b1e549fd6b38c53
govindhan-ravi <govindhanr7867@gmail.com>
2026-01-08 22:12:27 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,17 +1,1 @@
-variable "aws_region" {
-  description = "AWS region to deploy resources"
-  type        = string
-  default     = "us-east-1"
-}
 
-variable "ami" {
-  type        = string
-  default     = "ami-0453ec754f44f9a4a"
-  description = "AMI ID"
-}
-
-variable "instance_type" {
-  type        = string
-  default     = "t2.micro"
-  description = "EC2 instance type"
-}
