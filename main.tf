036af522299e1c4bcd226aebb2805cd7eabfb148
govindhan-ravi <govindhanr7867@gmail.com>
2026-01-08 21:58:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,29 @@
+terraform {
+  cloud {
+    organization = "neela"
 
+    workspaces {
+      name = "tf-cloud-s3"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "myec2" {
+  ami           = var.ami
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "myec2"
+  }
+}
